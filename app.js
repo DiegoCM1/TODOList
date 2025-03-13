e0781b21f42b5e623de08134faf410e96928cf67
Diego Colín <luiscolin764@gmail.com>
2025-03-13 14:32:43 -0600

    Added Lucide Icons library to the project

--- app.js
+++ app.js
@@ -1,44 +1,40 @@
 //myButton
 const myButton = document.getElementById("btn");
 const btnClickCallback = () => {
-    alert("You clicked me!");
-    myButton.removeEventListener("click", btnClickCallback);
-}
+  alert("You clicked me!");
+  myButton.removeEventListener("click", btnClickCallback);
+};
 myButton.style.backgroundColor = "gray";
 
 myButton.addEventListener("mouseover", () => {
-    myButton.style.backgroundColor = "red";
-})
+  myButton.style.backgroundColor = "red";
+});
 myButton.addEventListener("mouseout", () => {
-    myButton.style.backgroundColor = "gray";
-})
+  myButton.style.backgroundColor = "gray";
+});
 
 myButton.addEventListener("click", btnClickCallback);
 
 //Inputs and creation of elements
-const newInputElement = document.createElement("input");
-newInputElement.textContent = "Fue creado con createElement";
-const Container = document.querySelector(".container")
+const Container = document.querySelector(".container"); //Selected the container to add/remove the elements
 
 //Add
 const myButtonAdd = document.getElementById("btnAdd");
-const AddClickCallback = () => { //Function to add new elements
-    Container.appendChild(newInputElement);
-    Container.appendChild(document.createElement("br"));
-}
+const AddClickCallback = () => {
+  //Function to create and insert new input elements
+  const newInputElement = document.createElement("input"); //Create new input element
+  newInputElement.placeholder = "This is a new fucking task"; //Set placeholder
+  Container.appendChild(newInputElement);
+  Container.appendChild(document.createElement("br"));
+};
 
 myButtonAdd.addEventListener("click", AddClickCallback);
 
-
-
 //Substract - Remove last element
 const myButtonSubstract = document.getElementById("btnSubstract");
-const SubstractClickCallback = () => { //Function to remove last elements
-    Container.removeChild(Container.lastChild);
-    Container.removeChild(Container.lastChild);
-}
+const SubstractClickCallback = () => {
+  //Function to remove last elements
+  Container.removeChild(Container.lastChild);
+  Container.removeChild(Container.lastChild);
+};
 myButtonSubstract.addEventListener("click", SubstractClickCallback);
-
-
-
-
