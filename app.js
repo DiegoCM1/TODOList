dca3162ab736a6738412e65982cb3b7f12ade43b
Diego Colín <104664207+DiegoCM1@users.noreply.github.com>
2025-03-13 13:15:42 -0600

    Added last files
    Added the files that started the project

---
+++ app.js
@@ -0,0 +1,29 @@
+//myButton
+const myButton = document.getElementById("btn");
+const btnClickCallback = () => {
+    alert("You clicked me!");
+    myButton.removeEventListener("click", btnClickCallback);
+}
+myButton.style.backgroundColor = "gray";
+
+myButton.addEventListener("mouseover", () => {
+    myButton.style.backgroundColor = "red";
+})
+myButton.addEventListener("mouseout", () => {
+    myButton.style.backgroundColor = "gray";
+})
+
+myButton.addEventListener("click", btnClickCallback);
+
+
+//Substract
+const myButtonSubstract = document.getElementById("btnSubstract");
+
+
+//Add
+const myButtonAdd = document.getElementById("btnAdd");
+
+
+
+
+console.log(myButton);
