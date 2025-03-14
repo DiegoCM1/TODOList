a93847d9256f534689de232fa219122ef4619eab
Diego Colín <luiscolin764@gmail.com>
2025-03-13 20:41:16 -0600

    Created a nav menu for different lists of tasks

--- app.js
+++ app.js
@@ -38,3 +38,43 @@ const SubstractClickCallback = () => {
   Container.removeChild(Container.lastChild);
 };
 myButtonSubstract.addEventListener("click", SubstractClickCallback);
+
+//Sections
+const starredList = document.querySelector("#starredList"); //Select the starred list
+
+const dailyList = document.querySelector("#dailyList"); //Select the daily list 
+dailyList.style.display = "none"; //Hide the daily list at beginning
+
+const newList = document.querySelector("#newList"); //Select the new list
+newList.style.display = "none"; //Hide the new list at beginning
+
+
+//Logic for showing sections lists
+const starredListButton = document.querySelector("#starredListButton"); //Select button to show starred list
+const dailyListButton = document.querySelector("#dailyListButton"); //Select button to show daily list
+const newListButton = document.querySelector("#newListButton"); //Select button to show new list
+
+const showStarredList = () => { //Function to show the starred list
+  starredList.style.display = "flex"; //Show the starred list
+  dailyList.style.display = "none"; //Hide the daily list
+  newList.style.display = "none"; //Hide the new list
+}
+
+const showDailyList = () => { //Function to show the daily list
+  starredList.style.display = "none"; //Hide the starred list
+  dailyList.style.display = "flex"; //Show the daily list
+  newList.style.display = "none"; //Hide the new list
+}
+
+const showNewList = () => { //Function to show the new list
+  starredList.style.display = "none"; //Hide the starred list
+  dailyList.style.display = "none"; //Hide the daily list
+  newList.style.display = "flex"; //Show the new list
+}
+
+starredListButton.addEventListener("click", showStarredList); //Add event listener to the starred list button
+dailyListButton.addEventListener("click", showDailyList); //Add event listener to the daily list button
+newListButton.addEventListener("click", showNewList); //Add event listener to the new list button
+
+
+
