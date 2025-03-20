07747bceb33c06aae931f1f64dba166857b95a82
Diego Colín <luiscolin764@gmail.com>
2025-03-20 15:30:27 -0600

    Created logic to add new tasks

--- app.js
+++ app.js
@@ -42,39 +42,74 @@ myButtonSubstract.addEventListener("click", SubstractClickCallback);
 //Sections
 const starredList = document.querySelector("#starredList"); //Select the starred list
 
-const dailyList = document.querySelector("#dailyList"); //Select the daily list 
+const dailyList = document.querySelector("#dailyList"); //Select the daily list
 dailyList.style.display = "none"; //Hide the daily list at beginning
 
 const newList = document.querySelector("#newList"); //Select the new list
 newList.style.display = "none"; //Hide the new list at beginning
-
 
 //Logic for showing sections lists
 const starredListButton = document.querySelector("#starredListButton"); //Select button to show starred list
 const dailyListButton = document.querySelector("#dailyListButton"); //Select button to show daily list
 const newListButton = document.querySelector("#newListButton"); //Select button to show new list
 
-const showStarredList = () => { //Function to show the starred list
+const showStarredList = () => {
+  //Function to show the starred list
   starredList.style.display = "flex"; //Show the starred list
   dailyList.style.display = "none"; //Hide the daily list
   newList.style.display = "none"; //Hide the new list
-}
+};
 
-const showDailyList = () => { //Function to show the daily list
+const showDailyList = () => {
+  //Function to show the daily list
   starredList.style.display = "none"; //Hide the starred list
   dailyList.style.display = "flex"; //Show the daily list
   newList.style.display = "none"; //Hide the new list
-}
+};
 
-const showNewList = () => { //Function to show the new list
+const showNewList = () => {
+  //Function to show the new list
   starredList.style.display = "none"; //Hide the starred list
   dailyList.style.display = "none"; //Hide the daily list
   newList.style.display = "flex"; //Show the new list
-}
+};
 
 starredListButton.addEventListener("click", showStarredList); //Add event listener to the starred list button
 dailyListButton.addEventListener("click", showDailyList); //Add event listener to the daily list button
 newListButton.addEventListener("click", showNewList); //Add event listener to the new list button
 
+//Class
+const button = document.querySelector("#button"); //Select the button
+
+const buttonClicked = (event) => {
+  console.log(event.target); //Calling the target of the event
+  console.log(event.target.id); //Calling the attributes of the event
+};
+
+button.addEventListener("click", buttonClicked); //Add event listener to the button
+
+//Class - Forms
+const form = document.getElementById("form"); //Select the form
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); //Prevent the default behavior of the form
+  const name = form.elements["name"].value;
+  console.log(name);
+}); //Add event listener to the form
 
 
+
+const formEx = document.querySelector("#task-form"); //Select the form
+const list = document.querySelector("#task-list"); //Select the list to create the tasks.
+
+formEx.addEventListener("submit", (e) => {
+  e.preventDefault(); //Prevents default beahviour
+  const inputEx = document.querySelector("#task-input") //Select the input
+
+  const newLi = document.createElement("li")//Create
+  newLi.textContent = inputEx.value//Edit
+  list.append(newLi)
+
+  console.log(inputEx.value) //Show the value of the input
+
+})
